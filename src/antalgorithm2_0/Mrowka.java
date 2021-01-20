d44ea9a9d3eb5fc2df395218a10986ddcac07ad5
Szymon Gołębiewski <szgolebiewski_256@wp.pl>
2021-01-20 20:29:16 +0100

    end 3.1

--- src/antalgorithm2_0/Mrowka.java
+++ src/antalgorithm2_0/Mrowka.java
@@ -10,21 +10,26 @@ public class Mrowka {
         // ilość pozostawianego feromonu w punkcie
     boolean syta;
     String sciezka="";
+    public int zjadaneJedzenie = (int) (Math.random() * (5 - 2) + 1);
+
+
     public Mrowka(World swiat){
-        this.swiat=swiat;}
+        this.swiat=swiat;
+    }
     private void policz_odleglosci(){
         // liczy odleglości od aktualnego położenia do pozostałych
         // punktów (oprócz własnego i mrowiska)
         int index=polozenie-'a';
         int dx,dy;
-        for(int i=0;i<swiat.lp;i++)
-          if((i==index)||(i==(swiat.home-'a')))
-            swiat.aux[i].ratio=MAX;
-          else{
-            dx=swiat.punkty[index].x-swiat.punkty[i].x;
-            dy=swiat.punkty[index].y-swiat.punkty[i].y;
-            swiat.aux[i].ratio=Math.sqrt(dx*dx+dy*dy);
-          }
+        for(int i=0;i<swiat.lp;i++){
+            if((i==index)||(i==(swiat.home-'a')))
+                swiat.aux[i].ratio=MAX;
+            else{
+                dx=swiat.punkty[index].x-swiat.punkty[i].x;
+                dy=swiat.punkty[index].y-swiat.punkty[i].y;
+                swiat.aux[i].ratio=Math.sqrt(dx*dx+dy*dy);
+            }
+        }
     }
     private void sortuj_tablice_pomocnicza(){
         // zapisuje w tablicy "aux" wartości
@@ -32,16 +37,19 @@ public class Mrowka {
         // a następnie sortuje je rosnąco
         for(int i=0;i<swiat.lp;i++)
            swiat.aux[i].ratio=(swiat.punkty[i].ferom+1)/swiat.aux[i].ratio;
+
         Auxil bufor=new Auxil();
-        for(int i=swiat.lp-1;i>0;i--)
-           for(int j=1;j<=i;j++)
-             if(swiat.aux[j].ratio>=swiat.aux[j-1].ratio){
-                bufor.name=swiat.aux[j].name;
-                bufor.ratio=swiat.aux[j].ratio;
-                swiat.aux[j].name=swiat.aux[j-1].name;
-                swiat.aux[j].ratio=swiat.aux[j-1].ratio;
-                swiat.aux[j-1].name=bufor.name;
-                swiat.aux[j-1].ratio=bufor.ratio;
+        for(int i=swiat.lp-1;i>0;i--) {
+            for (int j = 1; j <= i; j++) {
+                if (swiat.aux[j].ratio >= swiat.aux[j - 1].ratio) {
+                    bufor.name = swiat.aux[j].name;
+                    bufor.ratio = swiat.aux[j].ratio;
+                    swiat.aux[j].name = swiat.aux[j - 1].name;
+                    swiat.aux[j].ratio = swiat.aux[j - 1].ratio;
+                    swiat.aux[j - 1].name = bufor.name;
+                    swiat.aux[j - 1].ratio = bufor.ratio;
+                }
+            }
         }
     }
     private boolean sprawdz_sciezke(char znak){
@@ -60,10 +68,11 @@ public class Mrowka {
         double help[]; help=new double[swiat.lpw];
         double suma=0; double losowa=0;
         for(int i=0;i<swiat.lpw;i++)
-          suma+=swiat.aux[i].ratio;
-        for(int i=0;i<swiat.lpw;i++)
-        {help[i]=swiat.aux[i].ratio/suma;
-        losowa=Math.random();} 
+            suma+=swiat.aux[i].ratio;
+        for(int i=0;i<swiat.lpw;i++){
+            help[i]=swiat.aux[i].ratio/suma;
+            losowa=Math.random();
+        }
         int i=0; suma=help[i];
         while(losowa>suma)suma+=help[++i];
         return i;
@@ -72,15 +81,16 @@ public class Mrowka {
     public int wybierz_punkt(){ 
         // zwraca index kolejnego punktu
         for(int i=0;i<swiat.lp;i++)// resetuje tablicę pomocniczą
-          swiat.aux[i].name=swiat.punkty[i].name;
+            swiat.aux[i].name=swiat.punkty[i].name;
         policz_odleglosci();
         sortuj_tablice_pomocnicza();
         int wybor; boolean zly_wybor;
-      do{ 
-         wybor=ruletka();// wybór kolejnego punktu przy pomocy ruletki/losuj
-         zly_wybor=sprawdz_sciezke(swiat.aux[wybor].name);
-      }while(zly_wybor);
-    return wybor;
+
+        do{
+            wybor=ruletka();// wybór kolejnego punktu przy pomocy ruletki/losuj
+            zly_wybor=sprawdz_sciezke(swiat.aux[wybor].name);
+        }while(zly_wybor);
+        return wybor;
     }
 
     public void akcja(){
@@ -89,39 +99,43 @@ public class Mrowka {
         // jeśli TAK - dodaje do każdego punktu  na ścieżce feromon,
         //             i "usypia" mrówkę za pomocą "syta=true"
         int index=wybierz_punkt();
-     int valor=(swiat.aux[index].name!=(swiat.food))?1:0;
-     switch (valor)
-     { case 1: polozenie=swiat.aux[index].name;
+        int valor=(swiat.aux[index].name!=(swiat.food))?1:0;
+        switch (valor) {
+            case 1: polozenie=swiat.aux[index].name;
                sciezka+=polozenie;
                break;
-       case 0:
-           if (swiat.hmfood>0) {
-               syta=true;}
-           if (swiat.hmfood>=1){
-               swiat.hmfood = swiat.hmfood-1;}
+            case 0:
+                /*if (swiat.hmfood>0) {
+                    syta=true;
+                }*/
+                if (swiat.hmfood>=1){
+                    syta=true;
+                    swiat.hmfood = swiat.hmfood-zjadaneJedzenie;
+                }
 
+                polozenie=swiat.food;
+                sciezka+=polozenie;
+                polej_sciezke();
+                sciezka="";
+                polozenie=swiat.home;
 
-
-           polozenie=swiat.food;
-           sciezka+=polozenie;
-           polej_sciezke();
-           sciezka="";
-           polozenie=swiat.home;
-
-           /*polozenie=swiat.home;*/
-           System.out.println(sciezka+=polozenie);
-           //System.out.println(swiat.hmfood);
-
-           break;
-
-     }
+                /*polozenie=swiat.home;*/
+                System.out.println(sciezka+=polozenie);
+                System.out.println("hmfood: " + swiat.hmfood);
+                System.out.println("zjadaneJedzenie: " + zjadaneJedzenie);
+                //System.out.println(swiat.hmfood);
+                break;
+        }
     }
+
     void polej_sciezke() {
         // akcja wykonywana na punktach świata należących
         // do ścieżki tej mrówki, która doszła do pokarmu
+
+        //polanie w zaleznosci od ilsci pokarmu
         for (int i = 0; i < sciezka.length(); i++) {
             if (sciezka.charAt(i) != swiat.home) {
-                swiat.punkty[sciezka.charAt(i) - 'a'].ferom += feromon;
+                swiat.punkty[sciezka.charAt(i) - 'a'].ferom += zjadaneJedzenie;
             }
         }
     }
--- src/antalgorithm2_0/World.java
+++ src/antalgorithm2_0/World.java
@@ -11,49 +11,59 @@ public int lp;         // liczba punkt�w �wiata
         public char food;      // nazwa punktu zawieraj�cego pokarm
         public int lpw;        // liczba punkt�w wyboru dla mr�wek
         public int hmfood;     // ilość pokarmu
-    public float feromon;
-        int tabela[][]={{'a',6,1},{'b',13,1},{'c',4,3},{'d',4,5},{'e',8,5},
-                        {'f',6,8,},{'g',10,8}}; //konfiguracja �wiata     
+        public float feromon;
+        public Mrowka ant;
+        int tabela[][]={
+                {'a',6,1},
+                {'b',13,1},
+                {'c',4,3},
+                {'d',4,5},
+                {'e',8,5},
+                {'f',6,8,},
+                {'g',10,8}
+        }; //konfiguracja �wiata
     public World(int _lp, char _home, char _food, int _lpw, int _hmfood){
         lp=_lp; home=_home; food=_food; lpw=_lpw; hmfood=_hmfood;
         punkty=new Punkt[_lp];
         aux=new Auxil[_lp];
     }
-        void inicjuj(){
-            for(int i=0;i<lp;i++)
-             {punkty[i]=new Punkt();
-               punkty[i].name =(char)tabela[i][0];
-               punkty[i].x    =tabela[i][1];
-               punkty[i].y    =tabela[i][2];
-               punkty[i].ferom=0.0;
-              aux[i]=new Auxil();
-               aux[i].name='\0';
-               aux[i].ratio=0.0;
-             }
+    void inicjuj(){
+        for(int i=0;i<lp;i++) {
+            punkty[i]=new Punkt();
+            punkty[i].name =(char)tabela[i][0];
+            punkty[i].x    =tabela[i][1];
+            punkty[i].y    =tabela[i][2];
+            punkty[i].ferom=0.0;
+            aux[i]=new Auxil();
+            aux[i].name='\0';
+            aux[i].ratio=0.0;
         }
-        public void ferom_reset(){
-            // zeruje ilo�� foremonu we wszystkich punktach �wiata// pomniejsza ilość feromonu we wszystkich punktach świata.
-            for(int i=0;i<lp;i++) {
-                System.out.println(punkty[i].name + " " + punkty[i].ferom);
-                if (i == punkty[i].ferom || i < punkty[i].ferom) {
-                    float procentferomonu = (float) punkty[i].ferom/2;
-                    //procentferomonu=Math.round(procentferomonu);
-                    double roundOff = Math.round(procentferomonu*100.0)/100.0; // odcięcie tezaokrąglenie do dwóch po przecinku
-                    //System.out.println(roundOff);
-                    punkty[i].ferom = punkty[i].ferom - roundOff; // odjęcie reszty poza tym co jest do dwóch po przecinku.
-                }
+    }
+    public void ferom_reset(){
+        // zeruje ilo�� foremonu we wszystkich punktach �wiata// pomniejsza ilość feromonu we wszystkich punktach świata.
+        for(int i=0;i<lp;i++) {
+            System.out.println(punkty[i].name + " " + punkty[i].ferom);
+            if (i == punkty[i].ferom || i < punkty[i].ferom) {
+                float procentferomonu = (float) punkty[i].ferom/2;
+                //procentferomonu=Math.round(procentferomonu);
+                double roundOff = Math.round(procentferomonu*100.0)/100.0; // odcięcie tezaokrąglenie do dwóch po przecinku
+                //System.out.println(roundOff);
+                punkty[i].ferom = punkty[i].ferom - roundOff; // odjęcie reszty poza tym co jest do dwóch po przecinku.
+
             }
         }
-        public void pokaz () {
-            System.out.println();
-            for (int i = 0; i < lp; i++)
-                System.out.print((char) punkty[i].name + "   ");
-            System.out.println();
-            for (int i = 0; i < lp; i++)
-                System.out.print((float) punkty[i].ferom + "   ");
-            System.out.println();
-        }
-
     }
+
+    public void pokaz () {
+        System.out.println();
+        for (int i = 0; i < lp; i++)
+            System.out.print((char) punkty[i].name + "   ");
+        System.out.println();
+        for (int i = 0; i < lp; i++)
+            System.out.print((float) punkty[i].ferom + "   ");
+        System.out.println();
+    }
+
+}
 
 
